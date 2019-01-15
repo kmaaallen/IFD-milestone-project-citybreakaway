ac5b18dd083fbfc83fef864b7260ec01bbd00812
Kirsten Allen <kmaaallen@gmail.com>
2019-01-15 15:48:08 +0000

    rendered map at default start position Europe using Google API

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {lat: 51.748958, lng: 2.215369}
+    });
+}
+
+initMap();
